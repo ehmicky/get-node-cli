6b9d78d36fd0d6a2d3d3c0bc29d831cbc9e91ab5
ehmicky <ehmicky@gmail.com>
2021-10-31 17:46:32 +0100

    Fix read-pkg-up

--- src/bin.js
+++ src/bin.js
@@ -4,7 +4,7 @@ import { exit } from 'process'
 import { fileURLToPath } from 'url'
 
 import getNode from 'get-node'
-import { readPackageUpAsync } from 'read-pkg-up'
+import { readPackageUp } from 'read-pkg-up'
 import UpdateNotifier from 'update-notifier'
 
 import { parseOpts } from './parse.js'
@@ -28,7 +28,7 @@ const runCli = async function () {
 // TODO: use static JSON imports once those are possible
 const checkUpdate = async function () {
   const cwd = dirname(fileURLToPath(import.meta.url))
-  const { packageJson } = await readPackageUpAsync({ cwd, normalize: false })
+  const { packageJson } = await readPackageUp({ cwd, normalize: false })
   UpdateNotifier({ pkg: packageJson }).notify()
 }
 
